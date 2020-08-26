ded0785b8da52e122260c986d2406c7999741433
Esther Siebert <esther_siebert@live.nl>
2020-08-26 16:52:51 -0500

    CSS images and JS fade-in

---
+++ main.js
@@ -0,0 +1,31 @@
+//-----------------Fade-in items ----------------
+
+//Variables
+
+const faders = document.querySelectorAll('.fade-in');
+const appearOptions = {
+    threshold: 0.2
+};
+
+
+
+//Functions
+
+const appearOnScroll = new IntersectionObserver(function(entries, appearOnScroll) {
+    entries.forEach(entry => {
+        if (!entry.isIntersecting) {
+            console.log(`${entry} is not close yet`);
+            return;
+        } else {
+            console.log(`${entry} fading begins`);
+            entry.target.classList.add('appear');
+            appearOnScroll.unobserve(entry.target);
+        }
+    })
+}, appearOptions);
+
+faders.forEach(fader => {
+    appearOnScroll.observe(fader);
+});
+
+
